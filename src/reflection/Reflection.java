9f403772c9e2bb61833dc3209b976558cb2f9dc9
ambereye <287914967@qq.com>
2019-06-20 20:14:28 +0800

    add sout null

--- src/reflection/Reflection.java
+++ src/reflection/Reflection.java
@@ -4,7 +4,9 @@ package reflection;
 import dto.User;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 利用反射获取实体属性值
@@ -12,8 +14,7 @@ import java.util.Arrays;
  */
 public class Reflection {
 
-    public static boolean judgement(Object object,String[] arr) throws IllegalAccessException {
-        boolean flag = true;
+    public static boolean judgement(Object object,List<String> arr) throws IllegalAccessException {
 
         if (null != object) {
             Class<?> objectClass = object.getClass();
@@ -22,7 +23,7 @@ public class Reflection {
             for (Field field : fields) {
                 field.setAccessible(true);
                 System.out.println(field.getName());
-                if(Arrays.asList(arr).contains(field.getName())){
+                if(arr.contains(field.getName())){
                     Object value = field.get(object);
                     if (null == value || "".equals(value)) {
                         return false;
@@ -31,7 +32,7 @@ public class Reflection {
 
             }
         }
-        return flag;
+        return true;
     }
 
     public static void main(String[] args) throws IllegalAccessException {
@@ -40,7 +41,9 @@ public class Reflection {
         user.setSex("男");
         user.setAge(11);
         //user.setIdCard("111");
-        String[] arr ={"name","sex","age"};
+        //String[] arr ={"name","sex","age"};
+        List<String> arr = new ArrayList<>(Arrays.asList("name","sex","age"));
+        List<String> arr1 =Arrays.asList("name","sex","age");
         System.out.println(judgement(user,arr));
     }
 }
---
+++ src/stringOperate/Stringdemo.java
@@ -0,0 +1,15 @@
+package stringOperate;
+
+/**
+ * @author ambereye
+ * @desc
+ * @date 2019/6/20 19:44
+ */
+public class Stringdemo {
+
+    public static void main(String[] args) {
+        String a =null +"1";
+        System.out.println(a);
+    }
+
+}
